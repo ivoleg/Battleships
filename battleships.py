f680210b9a7721f7b9b6d899cf6cc32ddb10d092
ivoleg <ivol09@yandex.ru>
2020-11-05 12:00:58 +0000

    first working version

--- battleships.py
+++ battleships.py
@@ -1,5 +1,6 @@
-#see the readme.md file for description and data 
-
+### see the readme.md file for description and data 
+import copy
+import random
 
 def is_sunk(ship):
     if ship[3] == len(ship[4]):
@@ -17,55 +18,101 @@ def ship_type(ship):
     else:
         return 'submarine'
 
-def filled_squares(ship): #function find and store as set of tuples all the squares of the given ship and its adjacent squares
+def used_squares(ship): #function find and store as set of tuples all the squares of the given ship and its adjacent squares
     set_of_squares = set()
     if ship[2] == True:
         for i in range(ship[0] - 1, ship[0] + 2):
             for j in range(ship[1] - 1, ship[1] + ship[3] + 1):
-                square = (i,j)
-                set_of_squares.add(square)
-    elif ship[2] == False:
+                set_of_squares.add((i, j))
+    else:
         for i in range(ship[0] - 1, ship[0] + ship[3] + 1):
             for j in range(ship[1] - 1, ship[1] + 2):
-                square = (i,j)
-                set_of_squares.add(square)
+                set_of_squares.add((i, j))
     return set_of_squares
 
 def is_open_sea(row, column, fleet):
     square = (row, column)
-    all_filled_squares = set()
+    all_used_squares = set()
     for ship in fleet:
-        ship_squares = filled_squares(ship)
-        all_filled_squares.update(ship_squares)
-    if square in all_filled_squares:
+        used_ship_squares = used_squares(ship)
+        all_used_squares.update(used_ship_squares)
+    if square in all_used_squares:
         return False
     else:
         return True
 
+def ship_squares(ship): #function stores only ship squares as a set of tuples
+    ship_sq = set()
+    if ship[2] == True:
+        for i in range(ship[1], ship[1] + ship[3]):
+            ship_sq.add((ship[0], i))
+    else:
+        for i in range(ship[0], ship[0] + ship[3]):
+            ship_sq.add((i, ship[1]))
+    return ship_sq
 
+def fleet_squares(fleet): #function stores fleet as a set of tuples
+    fleet_sq = set()
+    for ship in fleet:
+        fleet_sq.update(ship_squares(ship))
+    return fleet_sq
+    
 def ok_to_place_ship_at(row, column, horizontal, length, fleet):
-    #remove pass and add your implementation
-    pass
+    ship = (row, column, horizontal, length, set())
+    if used_squares(ship).intersection(fleet_squares(fleet)) == set():
+        return True
+    else:
+        return False
 
 def place_ship_at(row, column, horizontal, length, fleet):
-    #remove pass and add your implementation
-    pass
+    new_fleet = copy.deepcopy(fleet)
+    new_fleet.append((row, column, horizontal, length, set()))
+    return new_fleet
 
 def randomly_place_all_ships():
-    #remove pass and add your implementation
-    pass
+    fleet = []
+    for i in range(4, 0, -1):
+        for j in range(1, 6 - i):
+            placed = False
+            while not placed:
+                horizontal = bool(random.getrandbits(1))
+                if horizontal == True:
+                    row = random.randint(0, 9)
+                    column = random.randint(0, 10 - i)
+                else:
+                    row = random.randint(0, 10 - i)
+                    column = random.randint(0, 9)
+                length = i
+                hits = set()
+                if ok_to_place_ship_at(row, column, horizontal, length, fleet):
+                    fleet = place_ship_at(row, column, horizontal, length, fleet)
+                    placed = True
+    return fleet
 
 def check_if_hits(row, column, fleet):
-    #remove pass and add your implementation
-    pass
+    hit_square = (row, column)
+    if hit_square in fleet_squares(fleet):
+        return True
+    else:
+        return False
 
 def hit(row, column, fleet):
-    #remove pass and add your implementation
-    pass
-
+    for ship in fleet:
+        if (row, column) in ship_squares(ship) and (row, column) not in ship[4]:
+            ship_hit = ship
+    old_ship = copy.deepcopy(ship_hit)
+    ship_hit[4].add((row, column))
+    return (fleet, old_ship)
+        
 def are_unsunk_ships_left(fleet):
-    #remove pass and add your implementation
-    pass
+    counter = 0
+    for ship in fleet:
+        if is_sunk(ship):
+            counter+=1
+    if counter == 10:
+        return False
+    else:
+        return True
 
 def main():
     #the implementation provided below is indicative only
@@ -88,7 +135,7 @@ def main():
         else:
             print("You missed!")
 
-        if not are_unsunk_shis_left(current_fleet): game_over = True
+        if not are_unsunk_ships_left(current_fleet): game_over = True
 
     print("Game over! You required", shots, "shots.")
 
